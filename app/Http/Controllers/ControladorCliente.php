8ea84455d590e265fbd88ee3f1d1e934cdf00043
Anatoliy Stryzhekozin <camandatsjw@gmail.com>
2021-08-11 20:38:37 -0300

    5

---
+++ app/Http/Controllers/ControladorCliente.php
@@ -0,0 +1,48 @@
+<?php 
+
+
+
+namespace App\Http\Controllers;
+
+use App\Entidades\Sistema\Menu; //include_once "app/Entidades/Sistema/Menu.php";
+use App\Entidades\Sistema\MenuArea;
+use App\Entidades\Sistema\Patente;
+use App\Entidades\Sistema\Usuario;
+use Illuminate\Http\Request;
+
+class ControladorMenu extends Controller
+{
+    public function index()
+    {
+        $titulo = "Listado clientes";
+        if (Usuario::autenticado() == true) {
+            if (!Patente::autorizarOperacion("MENUCONSULTA")) {
+                $codigo = "MENUCONSULTA";
+                $mensaje = "No tiene permisos para la operaci&oacute;n.";
+                return view('sistema.pagina-error', compact('titulo', 'codigo', 'mensaje'));
+            } else {
+                return view('cliente.cliente-listar', compact('titulo')); //crear carpeta
+            }
+        } else {
+            return redirect('admin/login');
+        }
+    }
+
+    
+
+    public function nuevo()
+    {
+        $titulo = "Nuevo Listado clientes";
+
+        $entidad = new Cliente();
+        $array_menu = $entidad->obtenerMenuPadre();
+
+        return view('sistema.menu-nuevo', compact('titulo', 'array_menu'));
+
+    }
+
+    
+  
+}
+
+ ?>
