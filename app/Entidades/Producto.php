35916425feae49acc5b484706d77935eea79de11
mtedeschini <matiastedeschini@gmail.com>
2021-08-11 19:19:36 -0300

    Cambios

--- app/Entidades/Producto.php
+++ app/Entidades/Producto.php
@@ -29,6 +29,59 @@ class Producto extends Model
         $lstRetorno = DB::select($sql);
         return $lstRetorno;
     }
+
+    
+    public function obtenerPorId($idproducto)
+    {
+        $sql = "SELECT
+                    idproducto,
+                    nombre,
+                    precio,
+                    descripcion                
+                FROM productos WHERE idproducto = $idproducto";
+        $lstRetorno = DB::select($sql);
+
+        if (count($lstRetorno) > 0) {
+            $this->idproducto = $lstRetorno[0]->idproducto;
+            $this->nombre = $lstRetorno[0]->nombre;
+            $this->precio = $lstRetorno[0]->precio;
+            $this->descripcion = $lstRetorno[0]->descripcion;
+            return $this;
+        }
+        return null;
+    }
+
+    public function guardar() {
+        $sql = "UPDATE productos SET
+            nombre='$this->nombre',
+            precio=$this->precio,
+            descripcion='$this->descripcion'
+            WHERE idproducto=?";
+        $affected = DB::update($sql, [$this->idproducto]);
+    }
+
+    public function eliminar()
+    {
+        $sql = "DELETE FROM productos WHERE
+            idproducto=?";
+        $affected = DB::delete($sql, [$this->idproducto]);
+    }
+
+    public function insertar()
+    {
+        $sql = "INSERT INTO productos (
+                nombre,
+                precio,
+                descripcion
+            ) VALUES (?, ?, ?);";
+        $result = DB::insert($sql, [
+            $this->nombre,
+            $this->precio,
+            $this->descripcion
+        ]);
+        return $this->idproducto = DB::getPdo()->lastInsertId();
+    }
+
 }
 
 ?>
