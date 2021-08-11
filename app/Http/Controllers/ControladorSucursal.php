fd12c8c40ee74a08bb7e3e29cb9cec2b2f5b8bf6
mtfc99 <martincosso99@hotmail.com>
2021-08-11 20:15:55 -0300

    cambios

--- app/Http/Controllers/ControladorSucursal.php
+++ app/Http/Controllers/ControladorSucursal.php
@@ -3,25 +3,34 @@
 namespace App\Http\Controllers;
 
 use App\Entidades\Sucursal;
+use App\Entidades\Sistema\Usuario;
+
 
 
 class ControladorSucursal extends Controller{
 
-
-
-
-
-
-
-
-
+    public function index()
+    {
+        $titulo = "Sucursales";
+        if (Usuario::autenticado() == true) {
+            if (!SUCURSAL::autorizarOperacion("SUCURSALCONSULTA")) {
+                $codigo = "SUCURSALCONSULTA";
+                $mensaje = "No tiene permisos para la operaci&oacute;n.";
+                return view('sistema.pagina-error', compact('titulo', 'codigo', 'mensaje'));
+            } else {
+                return view('sistema.sucursal-listar', compact('titulo'));
+            }
+        } else {
+            return redirect('admin/login');
+        }
+    }
 
     public function nuevo()
     {
         $titulo = "Nueva Sucursal";
         if (Usuario::autenticado() == true) {
-            if (!Sucursal::autorizarOperacion("NUEVASUCURSAL")) {
-                $codigo = "NUEVASUCURSAL";
+            if (!Sucursal::autorizarOperacion("SUCURSALALTA")) {
+                $codigo = "SUCURSALALTA";
                 $mensaje = "No tiene pemisos para la operaci&oacute;n.";
                 return view('sistema.pagina-error', compact('titulo', 'codigo', 'mensaje'));
             } else {
@@ -31,8 +40,6 @@ class ControladorSucursal extends Controller{
             return redirect('admin/login');
         }
     }
-
-
 }
 
 
