3e27e75a78ead9e50aa8bd752d558c214d1e38be
Anatoliy Stryzhekozin <camandatsjw@gmail.com>
2021-08-12 10:27:43 -0300

    6

--- routes/web.php
+++ routes/web.php
@@ -97,14 +97,14 @@ Route::group(array('domain' => '127.0.0.1'), function () {
     /* --------------------------------------------- */
     /* CONTROLADOR CLIENTES                          */
     /* --------------------------------------------- */
-<<<<<<< HEAD
+
     
 
 
-    Route::get('/admin/cliente/listado', 'ControladorCliente@index');
-=======
-    Route::get('/admin/clientes', 'ControladorPedidos@index'); 
->>>>>>> 54e91452f8547f73bdc07fb0f88c9e6fda155a41
+    
+
+    Route::get('/admin/clientes', 'ControladorCliente@index');  
+
     Route::get('/admin/cliente/nuevo', 'ControladorCliente@nuevo');
 
     /* --------------------------------------------- */
