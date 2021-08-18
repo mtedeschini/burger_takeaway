33d6e68a932dde8ddfd6222cfe93c939c9346c91
mtedeschini <matiastedeschini@gmail.com>
2021-08-18 20:01:03 -0300

    Cambios

--- routes/web.php
+++ routes/web.php
@@ -119,6 +119,7 @@ Route::group(array('domain' => '127.0.0.1'), function () {
     /* --------------------------------------------- */
     Route::get('/admin/pedidos', 'ControladorPedido@index');
     Route::get('/admin/pedido/nuevo', 'ControladorPedido@nuevo');
+    Route::get('/admin/pedido/cargarGrilla', 'ControladorPedido@cargarGrilla')->name('pedido.cargarGrilla');
 
 
     /* --------------------------------------------- */
