eb808e028716582b9de6812ada02cbcae1bdc483
sergio7776 <sergioblancoor@gmail.com>
2021-08-18 20:29:00 -0300

    changes

--- app/Http/Controllers/ControladorSucursal.php
+++ app/Http/Controllers/ControladorSucursal.php
@@ -27,6 +27,38 @@ class ControladorSucursal extends Controller{
         }
     }
 
+    public function cargarGrilla()
+    {
+        $request = $_REQUEST;
+
+        $entidad = new Sucursal();
+        $aMenu = $entidad->obtenerFiltrado();
+
+        $data = array();
+        $cont = 0;
+
+        $inicio = $request['start'];
+        $registros_por_pagina = $request['length'];
+
+
+        for ($i = $inicio; $i < count($aSucursal) && $cont < $registros_por_pagina; $i++) {
+            $row = array();
+            $row[] = '<a href="/admin/sistema/sucursales/' . $aSucursal[$i]->idsucursal . '">' . $aSucursal[$i]->nombre . '</a>';
+            $row[] = $aSucursal[$i]->nombre;
+            $row[] = $aSucursal[$i]->direccion;
+            $cont++;
+            $data[] = $row;
+        }
+
+        $json_data = array(
+            "draw" => intval($request['draw']),
+            "recordsTotal" => count($aSucursal), //cantidad total de registros sin paginar
+            "recordsFiltered" => count($aSucursal), //cantidad total de registros en la paginacion
+            "data" => $data,
+        );
+        return json_encode($json_data);
+    }
+
     public function nuevo()
     {
         $titulo = "Nueva Sucursal";
