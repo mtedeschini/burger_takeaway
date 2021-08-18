285047b660ac50b1905899f6c09eff3b6936e06a
mtfc99 <martincosso99@hotmail.com>
2021-08-18 20:05:24 -0300

    cambios

--- app/Http/Controllers/ControladorSucursal.php
+++ app/Http/Controllers/ControladorSucursal.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Entidades\Sucursal;
 use App\Entidades\Sistema\Usuario;
 use App\Entidades\Sistema\Patente;
+use Illuminate\Http\Request;
 
 require app_path() . '/start/constants.php';
 
@@ -42,5 +43,63 @@ class ControladorSucursal extends Controller{
             return redirect('admin/login');
         }
     
-}
+    }
+
+    public function guardar(Request $request) {
+        try {
+            //Define la entidad servicio
+            $titulo = "Modificar menú";
+            $entidad = new Sucursal();
+            $entidad->cargarDesdeRequest($request);
+
+            //validaciones
+            if ($entidad->nombre == "") {
+                $msg["ESTADO"] = MSG_ERROR;
+                $msg["MSG"] = "Complete todos los datos";
+            } else {
+                if ($_POST["id"] > 0) {
+                    //Es actualizacion
+                    $entidad->guardar();
+
+                    $msg["ESTADO"] = MSG_SUCCESS;
+                    $msg["MSG"] = OKINSERT;
+                } else {
+                    //Es nuevo
+                    $entidad->insertar();
+
+                    $msg["ESTADO"] = MSG_SUCCESS;
+                    $msg["MSG"] = OKINSERT;
+                }
+                $menu_grupo = new MenuArea();
+                $menu_grupo->fk_idmenu = $entidad->idmenu;
+                $menu_grupo->eliminarPorMenu();
+                if ($request->input("chk_grupo") != null && count($request->input("chk_grupo")) > 0) {
+                    foreach ($request->input("chk_grupo") as $grupo_id) {
+                        $menu_grupo->fk_idarea = $grupo_id;
+                        $menu_grupo->insertar();
+                    }
+                }
+                $_POST["id"] = $entidad->idmenu;
+                return view('sistema.menu-listar', compact('titulo', 'msg'));
+            }
+        } catch (Exception $e) {
+            $msg["ESTADO"] = MSG_ERROR;
+            $msg["MSG"] = ERRORINSERT;
+        }
+
+        $id = $entidad->idmenu;
+        $menu = new Menu();
+        $menu->obtenerPorId($id);
+
+        $entidad = new Menu();
+        $array_menu = $entidad->obtenerMenuPadre($id);
+
+        $menu_grupo = new MenuArea();
+        $array_menu_grupo = $menu_grupo->obtenerPorMenu($id);
+
+        return view('sistema.menu-nuevo', compact('msg', 'menu', 'titulo', 'array_menu', 'array_menu_grupo')) . '?id=' . $menu->idmenu;
+
+    }
+
+
 }
