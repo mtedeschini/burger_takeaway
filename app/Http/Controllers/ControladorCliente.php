91213ebe8376ab031a40a2e7873297a4f9604411
alejandrodelcs <alejodc15@gmail.com>
2021-08-19 08:37:00 -0300

    nuevo

--- app/Http/Controllers/ControladorCliente.php
+++ app/Http/Controllers/ControladorCliente.php
@@ -43,38 +43,40 @@ class ControladorCliente extends Controller
     
   
 
-    public function guardar(Request $request) {
-    try {
-        //Define la entidad servicio
-        $titulo = "Modificar Cliente";
-        $entidad = new Cliente();
-        $entidad->cargarDesdeRequest($request);
+    public function guardar(Request $request) 
+    {
+        try {
+            //Define la entidad servicio
+            $titulo = "Modificar Cliente";
+            $entidad = new Cliente();
+            $entidad->cargarDesdeRequest($request);
 
-        //validaciones
-        if ($entidad->nombre == "") {
-            $msg["ESTADO"] = MSG_ERROR;
-            $msg["MSG"] = "Complete todos los datos";
-        } else {
-            if ($_POST["id"] > 0) {
-                //Es actualizacion
-                $entidad->guardar();
-
-                $msg["ESTADO"] = MSG_SUCCESS;
-                $msg["MSG"] = OKINSERT;
+            //validaciones
+            if ($entidad->nombre == "") {
+                $msg["ESTADO"] = MSG_ERROR;
+                $msg["MSG"] = "Complete todos los datos";
             } else {
-                //Es nuevo
-                $entidad->insertar();
+                if ($_POST["id"] > 0) {
+                    //Es actualizacion
+                    $entidad->guardar();
 
-                $msg["ESTADO"] = MSG_SUCCESS;
-                $msg["MSG"] = OKINSERT;
+                    $msg["ESTADO"] = MSG_SUCCESS;
+                    $msg["MSG"] = OKINSERT;
+                } else {
+                    //Es nuevo
+                    $entidad->insertar();
+
+                    $msg["ESTADO"] = MSG_SUCCESS;
+                    $msg["MSG"] = OKINSERT;
+                }
+                
+                $_POST["id"] = $entidad->idcliente;
+                return view('sistema.cliente-listar', compact('titulo', 'msg'));
             }
-            
-            $_POST["id"] = $entidad->idcliente;
-            return view('sistema.cliente-listar', compact('titulo', 'msg'));
+        } catch (Exception $e) {
+            $msg["ESTADO"] = MSG_ERROR;
+            $msg["MSG"] = ERRORINSERT;
         }
-    } catch (Exception $e) {
-        $msg["ESTADO"] = MSG_ERROR;
-        $msg["MSG"] = ERRORINSERT;
     }
 
     public function cargarGrilla()
@@ -113,4 +115,4 @@ class ControladorCliente extends Controller
         return json_encode($json_data);
     }
 }
- ?>
+?>
