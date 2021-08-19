b0a3e4229124f96770aae055d68e4536296ec687
alejandrodelcs <alejodc15@gmail.com>
2021-08-19 00:00:10 -0300

    cambios

--- app/Http/Controllers/ControladorPedido.php
+++ app/Http/Controllers/ControladorPedido.php
@@ -48,10 +48,6 @@ class ControladorPedido extends Controller
                     //Es actualizacion
                     $entidadPedido->guardar();
 
-                    //Actualiza en datos personales
-                    //$legajo = new Personal();
-                    //$legajo->guardarDesdeUsuario($entidadUsuario);
-
                 } else {
                     //Inserta en datos personales
                     //$legajo = new Personal();
@@ -63,43 +59,7 @@ class ControladorPedido extends Controller
                     $_POST["id"] = $entidadPedido->idpedido;
                 }
 
-                if (Patente::autorizarOperacion("USUARIOAGREGARPERMISO")) {
-
-                    //Guarda las patentes de la familia
-                    $familiaUsuario = new Usuario_familia();
-
-                    //Elimina todos las familias del usuario
-                    $aFamiliaAsignados = $familiaUsuario->eliminarPorUsuario($entidadUsuario->idusuario);
-
-                    //Obtiene las familias a asignar al usuario
-                    $aFamiliaSinAsignar = array();
-                    foreach ($_POST as $nombre => $valor) {
-                        if (substr($nombre,0,12) == "chk_Familia_") {
-                        	$idCompuesto = explode("_", substr($nombre,12,strlen($nombre)));
-                            $idFamilia = $idCompuesto[0];
-                            $idArea = $idCompuesto[1];
-                            $familiaUsuario->fk_idfamilia = $idFamilia;
-                            $familiaUsuario->fk_idarea = $idArea;
-                            $familiaUsuario->fk_idusuario = $entidadUsuario->idusuario;
-                        	$familiaUsuario->insertar();
-                        }
-                    }
-                }
-                $msg["ESTADO"] = MSG_SUCCESS;
-                $msg["MSG"] = OKINSERT;
-                return view('sistema.listar', compact('titulo', 'msg'));
             }
-            $usuario = new Usuario();
-            $usuario->obtenerPorUsuario($request->input('txtUsuario'));
-            //return view('sistema.nuevo-usuario', compact('msg', 'usuario')) . '?id' . $request->input('txtUsuario');
-
-            $area = new Area();
-            $array_area =  $area->obtenerTodos();
-
-            $grupo = new Area();
-            $array_grupo = $grupo->obtenerTodos();
-
-            return view('sistema.nuevo-usuario', compact('array_area','msg','usuario', 'array_grupo')). '?id' . $request->input('txtUsuario');
         } catch (Exception $e) {
             $msg["ESTADO"] = MSG_ERROR;
             $msg["MSG"] = ERRORINSERT;
