4502340227ab692f7e4891bfdac8b81701d0db30
Anatoliy Stryzhekozin <camandatsjw@gmail.com>
2021-08-20 12:21:08 -0300

    18

--- app/Http/Controllers/ControladorCliente.php
+++ app/Http/Controllers/ControladorCliente.php
@@ -34,48 +34,14 @@ class ControladorCliente extends Controller
         return view('cliente.cliente-nuevo', compact('titulo') );
     }
 
-    public function guardar(Request $request) 
-    {
-        try {
-            //Define la entidad servicio
-            $titulo = "Modificar Cliente";
-            $entidad = new Cliente();
-            $entidad->cargarDesdeRequest($request);
-
-            //validaciones
-            if ($entidad->nombre == "") {
-                $msg["ESTADO"] = MSG_ERROR;
-                $msg["MSG"] = "Complete todos los datos";
-            } else {
-                if ($_POST["id"] > 0) {
-                    //Es actualizacion
-                    $entidad->guardar();
-
-                    $msg["ESTADO"] = MSG_SUCCESS;
-                    $msg["MSG"] = OKINSERT;
-                } else {
-                    //Es nuevo
-                    $entidad->insertar();
-
-                    $msg["ESTADO"] = MSG_SUCCESS;
-                    $msg["MSG"] = OKINSERT;
-                }
-                
-                $_POST["id"] = $entidad->idcliente;
-                return view('sistema.cliente-listar', compact('titulo', 'msg'));
-            }
-        } catch (Exception $e) {
-            $msg["ESTADO"] = MSG_ERROR;
-            $msg["MSG"] = ERRORINSERT;
-        }
-    }
+  
 
     public function cargarGrilla()
     {
         $request = $_REQUEST;
 
         $entidad = new Cliente();
-        $aClientes = $entidad->obtenerFiltrado();
+        $aClientes = $entidad->obtenerFiltrado(); 
 
         $data = array();
         $cont = 0;
@@ -114,6 +80,42 @@ class ControladorCliente extends Controller
         );
         return json_encode($json_data);
     }
+
+
+     public function guardar(Request $request) {
+    try { 
+        //Define la entidad servicio
+        $titulo = "Modificar cliente";
+        $entidad = new Cliente();
+        $entidad->cargarDesdeRequest($request); 
+
+        //validaciones
+        if ($entidad->nombre == "") {
+            $msg["ESTADO"] = MSG_ERROR;
+            $msg["MSG"] = "Complete todos los datos";
+        } else {
+            if ($_POST["id"] > 0) {
+                //Es actualizacion
+                $entidad->guardar();
+
+                $msg["ESTADO"] = MSG_SUCCESS;
+                $msg["MSG"] = OKINSERT;
+            } else {
+                //Es nuevo
+                $entidad->insertar();
+
+                $msg["ESTADO"] = MSG_SUCCESS;
+                $msg["MSG"] = OKINSERT;
+            }
+            
+            $_POST["id"] = $entidad->idcliente;
+            return view('sistema.cliente-listar', compact('titulo', 'msg'));
+        }
+    } catch (Exception $e) {
+        $msg["ESTADO"] = MSG_ERROR;
+        $msg["MSG"] = ERRORINSERT;
+    }
+}
 
 }
 ?>
--- routes/web.php
+++ routes/web.php
@@ -101,6 +101,7 @@ Route::group(array('domain' => '127.0.0.1'), function () {
        
     Route::get('/admin/clientes', 'ControladorCliente@index'); 
     Route::get('/admin/cliente/nuevo', 'ControladorCliente@nuevo');
+    Route::post('/admin/cliente/nuevo', 'ControladorCliente@guardar');
     Route::get('/admin/cliente/cargarGrilla', 'ControladorCliente@cargarGrilla')->name('cliente.cargarGrilla');   
 
 
