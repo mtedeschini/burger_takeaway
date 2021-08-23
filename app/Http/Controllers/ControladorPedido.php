80c1df02c6836b04569d70cec319048096a9a23d
milton38 <milton95-ba@hotmail.com>
2021-08-23 19:44:29 -0300

    cambios

--- app/Http/Controllers/ControladorPedido.php
+++ app/Http/Controllers/ControladorPedido.php
@@ -121,4 +121,27 @@ class ControladorPedido extends Controller
         return json_encode($json_data);
     }
 
+    public function eliminar(Request $request)
+    {
+        $id = $request->input('id');
+
+        if (Usuario::autenticado() == true) {
+            if (Patente::autorizarOperacion("PEDIDOLIMINAR")) {
+
+          
+                $entidad = new Pedido();
+                $entidad->cargarDesdeRequest($request);
+                $entidad->eliminar();
+
+                $aResultado["err"] = EXIT_SUCCESS; //eliminado correctamente
+            } else {
+                $codigo = "ELIMINARPROFESIONAL";
+                $aResultado["err"] = "No tiene pemisos para la operaci&oacute;n.";
+            }
+            echo json_encode($aResultado);
+        } else {
+            return redirect('admin/login');
+        }
+    }
+
 }
