f5ee247154f78921ed3e69a213ee7ecd53c90301
FRoberto24 <federoberto92@gmail.com>
2021-08-23 19:41:09 -0300

    cambios

--- app/Http/Controllers/ControladorProducto.php
+++ app/Http/Controllers/ControladorProducto.php
@@ -109,4 +109,6 @@ class ControladorProducto extends Controller{
 
         return view('producto.producto-nuevo', compact('msg', 'producto', 'titulo')) . '?id=' . $producto->idproducto;
     }
+
+    public function editar()
 }
