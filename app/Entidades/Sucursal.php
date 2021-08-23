b98ea970988a168256dea737569081e8682c7579
sergio7776 <sergioblancoor@gmail.com>
2021-08-23 02:44:40 -0300

    changes

--- app/Entidades/Sucursal.php
+++ app/Entidades/Sucursal.php
@@ -98,42 +98,6 @@ class Sucursal extends Model{
         return $lstRetorno;
     }
 
-    public function cargarGrilla()
-    {
-        $request = $_REQUEST;
-
-        $entidad = new Sucursal();
-        $aSucursales = $entidad->obtenerFiltrado();
-
-        $data = array();
-        $cont = 0;
-
-        $inicio = $request['start'];
-        $registros_por_pagina = $request['length'];
-
-
-        for ($i = $inicio; $i < count($aSucursales) && $cont < $registros_por_pagina; $i++) {
-            $row = array();
-
-            $row[] = '<a href="/admin/sucursal/' . $aSucursales[$i]->idsucursal . '">' . $aSucursales[$i]->nombre . '</a>';
-           
-            $row[] = $aSucursales[$i]->nombre;
-            $row[] = $aSucursales[$i]->direccion;
-            
-            $cont++;
-            $data[] = $row;
-        }
-
-        $json_data = array(
-            "draw" => intval($request['draw']),
-            
-            "recordsTotal" => count($aSucursales), //cantidad total de registros sin paginar
-            "recordsFiltered" => count($aSucursales), //cantidad total de registros en la paginacion
-            "data" => $data,
-        );
-        return json_encode($json_data);
-    }
-
     
 }
 
