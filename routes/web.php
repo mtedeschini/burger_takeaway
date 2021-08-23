e00e9bd99afac468520a6571be0b370f7fd2dd9c
ntarche <nelson.tarche@gmail.com>
2021-08-23 19:51:09 -0300

    cambios

--- routes/web.php
+++ routes/web.php
@@ -69,6 +69,7 @@ Route::group(array('domain' => '127.0.0.1'), function () {
     Route::get('/admin/usuarios/cargarGrilla', 'ControladorUsuario@cargarGrilla')->name('usuarios.cargarGrilla');
     Route::get('/admin/usuarios/buscarUsuario', 'ControladorUsuario@buscarUsuario');
     Route::get('/admin/usuarios/{usuario}', 'ControladorUsuario@editar');
+    Route::post('/admin/usuarios/{usuario}', 'ControladorUsuario@guardar');
 
     /* --------------------------------------------- */
     /* CONTROLADOR MENU                             */
@@ -105,6 +106,7 @@ Route::group(array('domain' => '127.0.0.1'), function () {
     Route::get('/admin/cliente/cargarGrilla', 'ControladorCliente@cargarGrilla')->name('cliente.cargarGrilla'); 
     Route::post('/admin/cliente/eliminar', 'ControladorCliente@eliminar');   
     Route::get('/admin/cliente/nuevo/{id}', 'Controladorcliente@editar');
+    Route::post('/admin/cliente/nuevo/{id}', 'Controladorcliente@guardar');
 
 
 
