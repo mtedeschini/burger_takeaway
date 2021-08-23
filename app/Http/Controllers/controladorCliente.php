a67c9514b9cbc4754f5b8b0b7539f5d79de196a4
nicolasagosti <agostinicolas6557@gmail.com>
2021-08-23 18:28:41 -0300

    cambios

--- app/Http/Controllers/controladorCliente.php
+++ app/Http/Controllers/controladorCliente.php
@@ -41,8 +41,42 @@ class ControladorCliente extends Controller
 
     }
 
-    
+}
   
+
+    public function guardar(Request $request) {
+    try {
+        //Define la entidad servicio
+        $titulo = "Modificar cliente";
+        $entidad = new Cliente();
+        $entidad->cargarDesdeRequest($request);
+
+        //validaciones
+        if ($entidad->nombre == "") {
+            $msg["ESTADO"] = MSG_ERROR;
+            $msg["MSG"] = "Complete todos los datos";
+        } else {
+            if ($_POST["id"] > 0) {
+                //Es actualizacion
+                $entidad->guardar();
+
+                $msg["ESTADO"] = MSG_SUCCESS;
+                $msg["MSG"] = OKINSERT;
+            } else {
+                //Es nuevo
+                $entidad->insertar();
+
+                $msg["ESTADO"] = MSG_SUCCESS;
+                $msg["MSG"] = OKINSERT;
+            }
+            
+            $_POST["id"] = $entidad->idcliente;
+            return view('sistema.cliente-listar', compact('titulo', 'msg'));
+        }
+    } catch (Exception $e) {
+        $msg["ESTADO"] = MSG_ERROR;
+        $msg["MSG"] = ERRORINSERT;
+    }
 }
 
  ?>
