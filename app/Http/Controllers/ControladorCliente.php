91eff1f1e53c6a160349a447b3c65e4fab73ece7
Anatoliy Stryzhekozin <camandatsjw@gmail.com>
2021-08-23 20:47:42 -0300

    lupa list

--- app/Http/Controllers/ControladorCliente.php
+++ app/Http/Controllers/ControladorCliente.php
@@ -58,7 +58,7 @@ class ControladorCliente extends Controller
         for ($i = $inicio; $i < count($aClientes) && $cont < $registros_por_pagina; $i++) {
             $row = array();
 
-            $row[] = '<a href="/admin/cliente/' . $aClientes[$i]->idcliente . '">' . $aClientes[$i]->nombre . '</a>'; 
+             $row[] = '<a href="/admin/cliente/' . $aClientes[$i]->idcliente . '" class="btn btn-secondary"><i class="fas  fa-search"></i></a>'; 
             
             $row[] = $aClientes[$i]->apellido;
             $row[] = $aClientes[$i]->telefono;
