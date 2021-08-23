475f094d37b2c754c389cc6096d6362465699961
sergio7776 <sergioblancoor@gmail.com>
2021-08-23 03:22:12 -0300

    changes

--- app/Entidades/Sucursal.php
+++ app/Entidades/Sucursal.php
@@ -76,9 +76,9 @@ class Sucursal extends Model{
     {
         $request = $_REQUEST;
         $columns = array(
-            0 => 'idsucursal',  
-            1 => 'nombre',  
-            2 => 'direccion'
+ 
+            0 => 'nombre',  
+            1 => 'direccion'
         );
         $sql = "SELECT DISTINCT
                     idsucursal,
