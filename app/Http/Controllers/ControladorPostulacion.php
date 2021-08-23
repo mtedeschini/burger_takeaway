58d70977ce588d1922d4d1437bc0acd097d419a0
rouale <roumel19@gmail.com>
2021-08-23 14:14:02 -0300

    cambios

--- app/Http/Controllers/ControladorPostulacion.php
+++ app/Http/Controllers/ControladorPostulacion.php
@@ -31,6 +31,51 @@ class ControladorPostulacion extends Controller{
 
     }
 
+    public function cargarGrilla()
+    {
+        $request = $_REQUEST;
+
+        $entidad = new Postulacion();
+        $aPostulaciones = $entidad->obtenerFiltrado(); 
+
+        $data = array();
+        $cont = 0;
+
+        $inicio = $request['start'];
+        $registros_por_pagina = $request['length'];
+
+
+        for ($i = $inicio; $i < count($aPostulaciones) && $cont < $registros_por_pagina; $i++) {
+            $row = array();
+
+            $row[] = '<a href="/admin/postulacion/' . $aPostulaciones[$i]->idcliente . '">' . $aPostulaciones[$i]->nombre . '</a>'; 
+            
+            $row[] = $aPostulaciones[$i]->apellido;
+            $row[] = $aPostulaciones[$i]->localidad;
+            $row[] = $aPostulaciones[$i]->documento;
+            $row[] = $aPostulaciones[$i]->correo;
+            $row[] = $aPostulaciones[$i]->telefono;
+            $row[] = $aPostulaciones[$i]->archivo_cv;
+
+           
+            $cont++;
+            $data[] = $row;
+        }
+
+        $json_data = array(
+            "draw" => intval($request['draw']),
+
+            "recordsTotal" => count($aClientes), //cantidad total de registros sin paginar
+            "recordsFiltered" => count($aClientes), //cantidad total de registros en la paginacion
+
+            "recordsTotal" => count($aClientes), //cantidad total de registros sin paginar
+            "recordsFiltered" => count($aClientes), //cantidad total de registros en la paginacion
+
+            "data" => $data,
+        );
+        return json_encode($json_data);
+    }
+
     public function guardar(Request $request) {
         try {
             //Define la entidad servicio
