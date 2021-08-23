92e1aa4e6982aa05c820a78c4ff2190ff1b569b6
rouale <roumel19@gmail.com>
2021-08-23 19:22:03 -0300

    cambios

--- routes/web.php
+++ routes/web.php
@@ -133,6 +133,7 @@ Route::group(array('domain' => '127.0.0.1'), function () {
     Route::get('/admin/postulacion/nuevo', 'ControladorPostulacion@nuevo');
     Route::post('/admin/postulacion/nuevo', 'ControladorPostulacion@guardar');
     Route::get('/admin/postulacion/cargarGrilla', 'ControladorPostulacion@cargarGrilla')->name('postulacion.cargarGrilla');
+    Route::get('/admin/postulacion/menu/{id}', 'ControladorPostulacion@editar');
 
     
     /* --------------------------------------------- */
