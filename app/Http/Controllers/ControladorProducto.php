e658861c22ce6d4beaaf0057185c5ecaf67ea966
valentinawilli <valentinawilli98@gmail.com>
2021-08-23 21:08:15 -0300

    cambios

--- app/Http/Controllers/ControladorProducto.php
+++ app/Http/Controllers/ControladorProducto.php
@@ -78,8 +78,8 @@ class ControladorProducto extends Controller{
         if (Usuario::autenticado() == true)
         
         {
-            if (!Patente::autorizarOperacion("MENUMODIFICACION")) {
-                $codigo = "MENUMODIFICACION";
+            if (!Patente::autorizarOperacion("PRODUCTOMODIFICACION")) {
+                $codigo = "PRODUCTOMODIFICACION";
                 $mensaje = "No tiene pemisos para la operaci&oacute;n.";
                 return view('sistema.pagina-error', compact('titulo', 'codigo', 'mensaje'));
             } else {
