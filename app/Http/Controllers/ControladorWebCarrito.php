8e3ece4f39b3a51126e2b241bfc24fb5ec4c582f
MaximoMephisto <sorondomaximo1@gmail.com>
2021-08-25 20:56:22 -0300

    Cambios

--- app/Http/Controllers/ControladorWebCarrito.php
+++ app/Http/Controllers/ControladorWebCarrito.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Entidades\Producto;
+
 class ControladorWebCarrito extends Controller {
 
     public function index() {
-        return view('web.carrito');
+        $producto = new Producto();
+        $aProductos = $producto->obtenerPorId();
+        return view('web.carrito', compact('aProductos'));
     }
 
 }
