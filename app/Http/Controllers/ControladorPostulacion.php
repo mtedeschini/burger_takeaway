d72b262e27f8c7ba41816a7b5d679eeda133fa1f
mtedeschini <matiastedeschini@gmail.com>
2021-08-24 21:17:18 -0300

    Cambios

--- app/Http/Controllers/ControladorPostulacion.php
+++ app/Http/Controllers/ControladorPostulacion.php
@@ -94,48 +94,6 @@ class ControladorPostulacion extends Controller{
         return json_encode($json_data);
     }
 
-    public function cargarGrilla()
-    {
-        $request = $_REQUEST;
-
-        $entidad = new Postulacion();
-        $aPostulaciones = $entidad->obtenerFiltrado(); 
-
-        $data = array();
-        $cont = 0;
-
-        $inicio = $request['start'];
-        $registros_por_pagina = $request['length'];
-
-
-        for ($i = $inicio; $i < count($aPostulaciones) && $cont < $registros_por_pagina; $i++) {
-            $row = array();
-
-            $row[] = '<a href="/admin/postulacion/' . $aPostulaciones[$i]->idpostulacion . '">' . $aPostulaciones[$i]->idpostulacion . '</a>'; 
-
-            $row[] = $aPostulaciones[$i]->nombre;
-            $row[] = $aPostulaciones[$i]->apellido;
-            $row[] = $aPostulaciones[$i]->localidad;
-            $row[] = $aPostulaciones[$i]->documento;
-            $row[] = $aPostulaciones[$i]->correo;
-            $row[] = $aPostulaciones[$i]->telefono;
-            $row[] = $aPostulaciones[$i]->archivo_cv;
-
-           
-            $cont++;
-            $data[] = $row;
-        }
-
-        $json_data = array(
-            "draw" => intval($request['draw']),
-
-            "recordsTotal" => count($aPostulaciones), //cantidad total de registros sin paginar
-            "recordsFiltered" => count($aPostulaciones), //cantidad total de registros en la paginacion
-            
-            "data" => $data,
-        );
-        return json_encode($json_data);
-    }
 
     public function guardar(Request $request) {
         try {
