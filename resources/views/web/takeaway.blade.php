9e620b6c8f131bbfca18257cb475cebbb133ceba
mtedeschini <matiastedeschini@gmail.com>
2021-08-25 19:46:29 -0300

    Cambios

--- resources/views/web/takeaway.blade.php
+++ resources/views/web/takeaway.blade.php
@@ -10,7 +10,7 @@
 
             <div class="col-md-7 col-sm-12 text-center ftco-animate">
             	<h1 class="mb-3 mt-5 bread">TAKE AWAY</h1>
-	            <p class="breadcrumbs"><span class="mr-2"><a href="index.php">Inicio</a></span></p>
+	            <p class="breadcrumbs"><span class="mr-2"><a href="/">Inicio</a></span></p>
             </div>
 
           </div>
