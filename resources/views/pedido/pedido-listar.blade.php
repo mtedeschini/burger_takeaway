47b3335c8418745979283b5ad04c3b7cb6b552eb
milton38 <milton95-ba@hotmail.com>
2021-08-25 20:47:34 -0300

    cambios

--- resources/views/pedido/pedido-listar.blade.php
+++ resources/views/pedido/pedido-listar.blade.php
@@ -13,7 +13,7 @@
 </ol>
 <ol class="toolbar">
     <li class="btn-item"><a title="Nuevo" href="/admin/pedido/nuevo" class="fa fa-plus-circle" aria-hidden="true"><span>Nuevo</span></a></li>
-    <li class="btn-item"><a title="Recargar" href="#" class="fa fa-refresh" aria-hidden="true" onclick='window.location.replace("/admin/pedidos");'><span>Recargar</span></a></li>
+    <li class="btn-item"><a title="Recargar" href="#" class="fa fa-refresh" aria-hidden="true" onclick='window.location.replace("/admin/pedido");'><span>Recargar</span></a></li>
 </ol>
 @endsection
 @section('contenido')
