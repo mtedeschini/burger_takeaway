d42cf03e1bbbbdec46289e95f081282c04b08f56
MaximoMephisto <sorondomaximo1@gmail.com>
2021-08-25 22:09:41 -0300

    Cambios

--- app/Http/Controllers/ControladorWebCarrito.php
+++ app/Http/Controllers/ControladorWebCarrito.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Entidades\Cliente;
 use App\Entidades\Producto;
+use App\Entidades\Sucursal;
 
 class ControladorWebCarrito extends Controller {
 
@@ -11,5 +13,16 @@ class ControladorWebCarrito extends Controller {
         $aProductos = $producto->obtenerTodos();
         return view('web.carrito', compact('aProductos'));
     }
+
+    public function nuevo() {
+        $titulo = "Pedido de carrito";
+        $entidadSucursal = new Sucursal();
+        $entidadCliente = new Cliente();
+        $aClientes = $entidadCliente->obtenerTodos();
+        $aSucursales = $entidadSucursal->obtenerTodos();
+        return view('pedido.pedido-nuevo', compact('titulo', 'aSucursales', 'aClientes'));
+    }
+
+
 
 }
