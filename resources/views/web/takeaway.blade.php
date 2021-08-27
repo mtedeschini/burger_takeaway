c73428f3b20de9a6fc554ce3b1a705943d4e9fdf
mtedeschini <matiastedeschini@gmail.com>
2021-08-27 12:30:19 -0300

    Cambios

--- resources/views/web/takeaway.blade.php
+++ resources/views/web/takeaway.blade.php
@@ -28,52 +28,36 @@
         </div>
     </div>
         <div class="container">
-            <div class="row no-gutters d-flex justify-content-center ">
-
-            <!--Comienzo Item Menu-->
-            @foreach ($aProductos as $producto)                            
-                    <div class="col-lg-4 mb-4 d-flex ftco-animate row" style="height: 400px">
-                        <div class="col-5">
-                            <img src="files/{{$producto->imagen}}" style="max-width: 250px; height: 400px; object-fit: cover" alt="">
-                        </div>
-                        <div class="text p-4 m-3 col-5" style="height: 250px;">
-                            <h3 >{{$producto->nombre}}</h3>
-                            <p > {{$producto->descripcion}}  </p>
-                            <p class="mb-0" style="color:orange">Precio: {{$producto->precio}} </p>
-                            <div class="form-group ">
-                                <label>Cantidad:</label>
-                                <input type="number" maxlength="50" id="txtCantidad" name="txtCantidad" class="form-control" placeholder="0" required>
-                            </div>
-                            <a href="#" class="ml-2 btn btn-white btn-outline-white">Agregar</a>
-                            </p>
-                        </div>
-                    </div>
-                    @endforeach
-            <!--Fin Item Menu-->
+            <div class="row d-flex justify-content-center ">
 
             <!--Comienzo Item Menu-->
             @foreach ($aProductos as $producto)         
-            <div class="col-md-4 col-12 p-sm-4 px-4 py-2">
-                <div class="row tarjeta">
-                    <div class="col-12 p-0">
-                        <img src="files/{{$producto->imagen}}" style="max-width: 330px; height: 400px; object-fit: cover" alt="">
+            <div class="col-md-6 col-xl-4 col-12 p-sm-4 px-4 py-2 ftco-animate">
+                <div class="row tarjeta d-flex justify-content-center">
+                    <div class="col-12 p-0" style="overflow:hidden; height: 300px">
+                        <img class="mx-auto d-block" src="files/{{$producto->imagen}}" style="height:340px;" alt="">
                         <!-- img-fluid para que se adapte la foto al tamaño del contenedor -->
                     </div>
-                    <div class="col-12 color-gradiente py-3 px-3 text-center" style="height: 100px;">
-                        <h3>{{$producto->nombre}}</h3>
-                    </div>
-                    <div class="col-12 text-center">
-                        <p>{{$producto->descripcion}} </p>
-                    </div>
-                    <div class="row pt-2 text-center">
-                        <div class="col-3 pt-3 pb-2">
-                          <p>Cantidad:</p>
+                    <div class="descripcion-items" style="background-color:rgb(18, 18, 18, .5);">
+                        <div class="col-12 color-gradiente pt-3 text-center">
+                            <h4>{{$producto->nombre}}</h4>
                         </div>
-                          <div class="col-3 pt-1 pb-2">
-                          <input type="number" maxlength="50" id="txtCantidad" name="txtCantidad" class="form-control" placeholder="0" required>
+                        <div class="col-12 text-center">
+                            <p>{{$producto->descripcion}} </p>
                         </div>
-                        <div class="col-6 pt-3 pb-2">
-                        <button><i class="icon-shopping-cart"></i> Agregar</button>
+                        <div class="col-12 text-center">
+                            <p class="precio-item"> Precio: ${{$producto->precio}}</p>
+                        </div>
+                        <div class="row pt-2 text-center d-flex justify-content-center">
+                            <div class="col-3 pt-3 pb-2">
+                                <p>Cantidad:</p>
+                            </div>
+                            <div class="col-3 pt-1 pb-2">
+                                <input type="number" min="0" maxlength="10" id="txtCantidad" name="txtCantidad" class="form-control" placeholder="0" required>
+                            </div>
+                            <div class="col-4 pt-3 pb-2">
+                                <button  utton class="btn btn-primary p-3 px-xl-4 py-xl-3"><i class="icon-shopping-cart"></i> Agregar</button>
+                            </div>
                         </div>
                     </div>
                 </div>
