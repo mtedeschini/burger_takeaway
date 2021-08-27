30009e1cebd1b11cea83f5e1640d8f52ce5e8bcd
nicolasagosti <agostinicolas6557@gmail.com>
2021-08-27 20:25:24 -0300

    cambios

--- resources/views/web/promociones.blade.php
+++ resources/views/web/promociones.blade.php
@@ -9,7 +9,7 @@
             <div class="row slider-text justify-content-center align-items-center">
 
                 <div class="col-md-7 col-sm-12 text-center ftco-animate">
-                    <h1 class="mb-3 mt-5 bread">Promos</h1>
+                    <h1 class="mb-3 mt-5 bread">Promociones</h1>
                     <p class="breadcrumbs"><span class="mr-2"><a href="/">Inicio</a></span></p>
                 </div>
 
