2a2d1de7e29720d15acfedc4f2ff4f7a7fad4e95
sergio7776 <sergioblancoor@gmail.com>
2021-08-28 12:05:06 -0300

    changes

--- resources/views/web/plantilla-sitio.blade.php
+++ resources/views/web/plantilla-sitio.blade.php
@@ -73,7 +73,7 @@
         
         <div class="col-lg-4 col-md-6 mb-5 mb-md-5">
           <div class="ftco-footer-widget mb-4">
-            <h2 class="ftco-heading-2">Recent Blog</h2>
+            <h2 class="ftco-heading-2">     Location</h2>
             <div class="block-21 mb-4 d-flex">
               <a class="blog-img mr-4" style="background-image: url(images/image_1.jpg);"></a>
               <div class="text">
