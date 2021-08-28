99ddcb733391ea2e355e2525d3e0e9bab30591e3
sergio7776 <sergioblancoor@gmail.com>
2021-08-28 12:00:24 -0300

    changes

--- app/Http/Controllers/ControladorWebHome.php
+++ app/Http/Controllers/ControladorWebHome.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Entidades\Sistema\Patente;
 use App\Entidades\Sistema\Usuario;
+use App\Entidades\Sucursal;
 
 class ControladorWebHome extends Controller
 {
-
-    public function index(){
-        return view('web.index');
+    public function index()
+    {
+    $sucursal = new Sucursal();
+    $aSucursales = $sucursal->obtenerTodos();
+    return view('web.index', compact('aSucursales'));
     }
 
 }
+
+
+
+
--- resources/views/web/plantilla-sitio.blade.php
+++ resources/views/web/plantilla-sitio.blade.php
@@ -109,13 +109,14 @@
             </ul>
           </div>
         </div>
-        
+
+        @foreach ($aSucursales as $sucursal) 
         <div class="col-lg-3 col-md-6 mb-5 mb-md-5">
           <div class="ftco-footer-widget mb-4">
             <h2 class="ftco-heading-2">Have a Questions?</h2>
             <div class="block-23 mb-3">
               <ul>
-                <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
+                <li><span class="icon icon-map-marker"></span><span class="text">{{$sucursal->nombre}}</span></li>
                 <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
                 <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
               </ul>
@@ -125,7 +126,9 @@
       </div>
       <div class="row">
         <div class="col-md-12 text-center">
+        @endforeach
 
+        
           <p>
             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
             Copyright &copy;<script>
