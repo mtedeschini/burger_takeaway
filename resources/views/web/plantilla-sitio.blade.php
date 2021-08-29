f4ec19b71a89bbb56e14be1d32d7b51c49dbb06a
alejandrodelcs <alejodc15@gmail.com>
2021-08-29 16:07:32 -0300

    cambios

--- resources/views/web/plantilla-sitio.blade.php
+++ resources/views/web/plantilla-sitio.blade.php
@@ -2,7 +2,7 @@
 <html lang="es">
 
 <head>
-  <title>hamburguesa - Free Bootstrap 4 Template by Colorlib</title>
+  <title>Burgers SRL</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
