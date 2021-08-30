3f9abe22cb9d06da6212342c38ce622bc80bfe70
ntarche <nelson.tarche@gmail.com>
2021-08-30 20:20:45 -0300

    cambios

--- app/Http/Controllers/ControladorWebCarrito.php
+++ app/Http/Controllers/ControladorWebCarrito.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Entidades\Carrito;
 use App\Entidades\Sucursal;
+use Illuminate\Http\Request;
 
 use MercadoPago\Item;
 use MercadoPago\MerchantOrder;
@@ -37,51 +38,5 @@ class ControladorWebCarrito extends Controller {
 
 
     }
-
-
-    public function guardar(Request $request){
-        try {
-            //Define la entidad servicio
-            $titulo = "Modificar Carrito";
-            $entidadCarrito = new Carrito();
-            $entidadCarrito->cargarDesdeRequest($request);
-
-            //validaciones
-            if ($entidadCarrito->fk_idproducto == ""){
-                $msg["ESTADO"] = MSG_ERROR;
-                $msg["MSG"] = "Complete todos los datos";
-            } else {
-                if ($_POST["id"] > 0) {
-                    //Es actualizacion
-                    $entidadCarrito->guardar();
-
-                    $msg["ESTADO"] = MSG_SUCCESS;
-                    $msg["MSG"] = OKINSERT;
-                    
-                } else {
-                    //es nuevo
-                    $entidadCarrito->insertar();
-
-                    $msg["ESTADO"] = MSG_SUCCESS;
-                    $msg["MSG"] = OKINSERT;        
-                }
-
-            
-                $_POST["id"] = $entidadCarrito->idcarrito;
-                return view('carrito.carrito-listar', compact('titulo','msg',));
-            }
-        } catch (Exception $e) {
-            $msg["ESTADO"] = MSG_ERROR;
-            $msg["MSG"] = ERRORINSERT;
-        }  
-        
-        $id = $entidadCarrito->idcarrito;
-        $carrito = new Carrito();
-
-        $carrito->obtenerPorId($id);
-
-        return view('carrito.carrito-nuevo', compact('msg', 'carrito', 'titulo')) . '?id=' . $carrito->idcarrito;
-    }        
-
 
 }
