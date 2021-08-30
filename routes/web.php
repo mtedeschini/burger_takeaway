3acc5d376203155ef266f0460a9566466cf1b91b
ntarche <nelson.tarche@gmail.com>
2021-08-30 20:18:11 -0300

    cambios

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::group(array('domain' => '127.0.0.1'), function () {
     Route::get('/', 'ControladorWebHome@index');
     Route::get('/nosotros', 'ControladorWebNosotros@index');
     Route::get('/takeaway', 'ControladorWebTakeaway@index');
-    Route::post('/takeaway', 'ControladorWebCarrito@guardar');
+    Route::post('/takeaway', 'ControladorWebTakeaway@guardar');
     Route::get('/promociones', 'ControladorWebpromociones@index');
     Route::get('/contacto', 'ControladorWebContacto@index');
     Route::get('/mi-cuenta', 'ControladorLogin@index');
