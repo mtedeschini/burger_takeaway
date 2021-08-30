e9f936acc0ad44da3689fde1f153556c8039fba9
mtfc99 <martincosso99@hotmail.com>
2021-08-30 20:51:25 -0300

    cambios

--- resources/views/web/promociones.blade.php
+++ resources/views/web/promociones.blade.php
@@ -9,7 +9,7 @@
             <div class="row slider-text justify-content-center align-items-center">
 
                 <div class="col-md-7 col-sm-12 text-center ftco-animate">
-                    <h1 class="mb-3 mt-5 bread">Promo</h1>
+                    <h1 class="mb-3 mt-5 bread">Promociones</h1>
                     <p class="breadcrumbs"><span class="mr-2"><a href="/">Inicio</a></span></p>
                 </div>
 
@@ -22,7 +22,7 @@
     <div class="container">
         <div class="row justify-content-center mb-5 pb-3">
             <div class="col-md-7 heading-section ftco-animate text-center">
-                <h2 class="mb-4">Promos </h2>
+                <h2 class="mb-4">Promos</h2>
                 <p>Estamos en C.A.B.A y G.B.A. Te esperamos en cualquiera de nuestras sucursales para retirar nuestras mejores hamburguesas.</p>
             </div>
         </div>
