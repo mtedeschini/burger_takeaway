7776fab33cec35ee73888438e2a690c956794235
mtfc99 <martincosso99@hotmail.com>
2021-09-01 20:11:02 -0300

    cambios

--- routes/web.php
+++ routes/web.php
@@ -133,7 +133,7 @@ Route::group(array('domain' => '127.0.0.1'), function () {
     /* --------------------------------------------- */
     Route::get('/admin/postulaciones', 'ControladorPostulacion@index');
     Route::get('/admin/postulacion/nuevo', 'ControladorPostulacion@nuevo');
-    Route::post('/admin/postulacion/nuevo', 'ControladorPostulacion@guardar')->name('postulacion.guardar');
+    Route::post('/admin/postulacion/nuevo', 'ControladorPostulacion@guardar')->name('postulacion.nuevo');
     Route::get('/admin/postulacion/cargarGrilla', 'ControladorPostulacion@cargarGrilla')->name('postulacion.cargarGrilla');
     Route::get('/admin/postulacion/eliminar', 'ControladorPostulacion@eliminar');
     Route::get('/admin/postulacion/{id}', 'ControladorPostulacion@editar');
