d7c595582a605601887ed314e6c7e97f1c3a8ff9
alejandrodelcs <alejodc15@gmail.com>
2021-09-06 18:53:30 -0300

    cambios

--- app/Http/Controllers/ControladorWebMiCuenta.php
+++ app/Http/Controllers/ControladorWebMiCuenta.php
@@ -2,25 +2,12 @@
 
 namespace App\Http\Controllers;
 use App\Entidades\Sucursal;
-<<<<<<< HEAD
-use App\Entidades\Cliente;
-=======
 use App\Entidades\Carrito;
->>>>>>> 3c1e3e91466d8b64c3fe957a11d577c6b3046bd1
 use Session;
 class ControladorWebMiCuenta extends Controller
 {
     public function index()
     {
-<<<<<<< HEAD
-        if(Session::get('cliente_id') != ""){  
-            $sucursal = new Sucursal();
-            $cliente = new Cliente();
-            $aClientes = $cliente->obtenerTodos();
-            $aSucursales = $sucursal->obtenerTodos();
-
-        return view('web.mi-cuenta', compact('aSucursales','aClientes'));
-=======
         if(Session::get('cliente_id') != ""){
         $carrito = new Carrito();
         $aCarritos = $carrito->obtenerPorCliente(Session::get('cliente_id'));
@@ -34,7 +21,6 @@ class ControladorWebMiCuenta extends Controller
         }
 
         return view('web.mi-cuenta', compact('aCarritos', 'aSucursales', 'productosCarrito'));
->>>>>>> 3c1e3e91466d8b64c3fe957a11d577c6b3046bd1
         } else {
             return redirect("/login");
         }
