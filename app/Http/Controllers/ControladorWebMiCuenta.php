4e53b9c38e6c4711e4457fa42d3595f3fbeee0ef
mtedeschini <matiastedeschini@gmail.com>
2021-09-06 13:34:24 -0300

    Cambios

--- app/Http/Controllers/ControladorWebMiCuenta.php
+++ app/Http/Controllers/ControladorWebMiCuenta.php
@@ -2,19 +2,27 @@
 
 namespace App\Http\Controllers;
 use App\Entidades\Sucursal;
+use App\Entidades\Carrito;
 use Session;
 class ControladorWebMiCuenta extends Controller
 {
-
     public function index()
     {
         if(Session::get('cliente_id') != ""){
+        $carrito = new Carrito();
+        $aCarritos = $carrito->obtenerPorCliente(Session::get('cliente_id'));
+
         $sucursal = new Sucursal();
         $aSucursales = $sucursal->obtenerTodos();
 
-        return view('web.mi-cuenta', compact('aSucursales'));
+        $productosCarrito = 0;
+        foreach ($aCarritos as $item){
+            $productosCarrito += $item->cantidad;
+        }
+
+        return view('web.mi-cuenta', compact('aCarritos', 'aSucursales', 'productosCarrito'));
         } else {
-            return redirect('/login');
+            return redirect("/login");
         }
     }
 
