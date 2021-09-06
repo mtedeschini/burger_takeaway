fad0dca9c7d1920dd29ef02707e4669988782fd6
alejandrodelcs <alejodc15@gmail.com>
2021-09-06 18:37:43 -0300

    cambios

--- app/Http/Controllers/ControladorWebMiCuenta.php
+++ app/Http/Controllers/ControladorWebMiCuenta.php
@@ -2,17 +2,20 @@
 
 namespace App\Http\Controllers;
 use App\Entidades\Sucursal;
+use App\Entidades\Cliente;
 use Session;
 class ControladorWebMiCuenta extends Controller
 {
 
     public function index()
     {
-        if(Session::get('cliente_id') != ""){
-        $sucursal = new Sucursal();
-        $aSucursales = $sucursal->obtenerTodos();
+        if(Session::get('cliente_id') != ""){  
+            $sucursal = new Sucursal();
+            $cliente = new Cliente();
+            $aClientes = $cliente->obtenerTodos();
+            $aSucursales = $sucursal->obtenerTodos();
 
-        return view('web.mi-cuenta', compact('aSucursales'));
+        return view('web.mi-cuenta', compact('aSucursales','aClientes'));
         } else {
             return redirect('/login');
         }
