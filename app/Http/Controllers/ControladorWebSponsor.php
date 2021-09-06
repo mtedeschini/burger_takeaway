63889b5b4b15e8c12ea2b95b3a339620b97d41b4
LucasKratos <lucas.spagnoli@gmail.com>
2021-09-06 12:55:06 -0300

    cambios

--- app/Http/Controllers/ControladorWebSponsor.php
+++ app/Http/Controllers/ControladorWebSponsor.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Entidades\Sponsor;
 use App\Entidades\Sucursal;
 use Illuminate\Http\Request;
 use Session;
@@ -11,10 +12,14 @@ class ControladorWebSponsor extends Controller
 
     public function index()
     {
+        $titulo = "Listado Sponsors";
         $sucursal = new Sucursal();
         $aSucursales = $sucursal->obtenerTodos();
+
+        $sponsor = new Sponsor();
+        $aSponsors = $sponsor->obtenerTodos();
         
-        return view('web.sponsor', compact( 'aSucursales'));
+        return view('web.sponsor', compact( 'titulo','aSucursales', 'aSponsors'));
     }
 
 }
