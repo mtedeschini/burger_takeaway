6c6247182164906a1531bf3411ccf8d08db5ec16
LucasKratos <lucas.spagnoli@gmail.com>
2021-09-08 10:10:24 -0300

    cambios

--- resources/views/web/sponsor.blade.php
+++ resources/views/web/sponsor.blade.php
@@ -11,7 +11,7 @@
                 <div class="col-md-7 col-sm-12 text-center ftco-animate">
                     <h1 class="mb-3 mt-5 bread">QUIERES SUMAR TUS PRODUCTOS A NUESTRA EMPRESA?</h1>
                     <h1 class="mb-3 mt-5 bread">Contactanos!</h1>
-                    <p class="breadcrumbs"><h4 class="mr-2"><a href="/">Inicio</h4></span>
+                    <p class="breadcrumbs"><h4 class="mr-2"><a href="/">Inicio</a></h4></span>
                     </p>
 
                 </div>
@@ -21,19 +21,19 @@
 </section>
 <div class="container">
             <div class="row d-flex justify-content-center mt-3 ">
-                <h1>Empresas que trabajan con nosotros</h1>
+                <h1>Nuestros Socios . . .</h1>
             </div>
-            <div class="row d-flex justify-content-center ">
+            <div class="row d-flex justify-content-center p-4 rounded" style="background: rgba(0,0,0,0.3);">
 
             <!--Comienzo Item Menu-->
            
             @foreach ($aSponsors as $sponsor)
             <div class="col-3 ftco-animate">
-                <div class="row  d-flex justify-content-center mt-3">
+                <div class="d-flex justify-content-around flex-nowrap">
                     <div class="descripcion-items" style="background-color:rgb(18, 18, 18, .5);">
                     
-                        <div class="col-12 p-0" style="overflow:hidden;">
-                             <img class="mx-auto d-block " src="files/{{$sponsor->foto_producto}}" style="height:10em; width:10em" alt="">
+                        <div class="col-12 p-0" >
+                             <img class="mx-auto d-block rounded m-4" src="files/{{$sponsor->foto_producto}}" style="height:13em; width:13em;" alt="">
                         </div>
                        
                     </div>
@@ -44,36 +44,36 @@
 
             </div>
         </div>
-<section class="ftco-section contact-section">
-    <div class="container mt-3">
-        <div class="row block-9 justify-content-around">
-            <div class="col-md-6 ftco-animate">
-                <form action="#" class="contact-form">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Nombre De la Empresa:">
+        <section class="ftco-section contact-section">
+            <div class="container mt-3">
+                <div class="row block-9 justify-content-around">
+                    <div class="col-md-6 ftco-animate">
+                        <form action="#" class="contact-form">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="Nombre De la Empresa:">
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="Email:">
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-md-12">
                             <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Email:">
+                                <input type="text" class="form-control" placeholder="Asunto:">
                             </div>
-                        </div>
+                            <div class="form-group">
+                                <textarea name="" id="" cols="30" rows="7" class="form-control"
+                                    placeholder="Mensaje:"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" value="Enviar" class="btn btn-primary py-3 px-5">
+                            </div>
+                        </form>
                     </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Asunto:">
-                    </div>
-                    <div class="form-group">
-                        <textarea name="" id="" cols="30" rows="7" class="form-control"
-                            placeholder="Mensaje:"></textarea>
-                    </div>
-                    <div class="form-group">
-                        <input type="submit" value="Enviar" class="btn btn-primary py-3 px-5">
-                    </div>
-                </form>
+                </div>
             </div>
-        </div>
-    </div>
-</section>
+        </section>
 @endsection
